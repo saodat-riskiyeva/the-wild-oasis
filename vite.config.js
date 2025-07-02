ff7a76abd55b30c2b6e472a59137e99ed228d8a6
Saodat <saodat.riskiyeva@gmail.com>
2025-07-02 12:32:44 -0400

    adding .env file for the data safety

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "process.env.SUPABASE_KEY": JSON.stringify(env.SUPABASE_KEY),
+    },
+    plugins: [react(), eslint()],
+  };
 });
