6e3f4bebcba78195c5b952c47450c88294ef7136
Saodat <saodat.riskiyeva@gmail.com>
2025-07-07 00:08:32 -0400

    uploading images to Supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -12,11 +12,37 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${process.env.SUPABASE_URL}/storage/v1/object/public/cabin-images/${imageName}`;
+  // https://hkwnzzjbycemwuuhxgsn.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
