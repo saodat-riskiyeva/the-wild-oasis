50bd7a8e27f1399a0ef7a721412addd090ced6d5
Saodat <saodat.riskiyeva@gmail.com>
2025-07-09 17:39:19 -0400

    minor code refactoring

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { createEditCabin } from "../../services/apiCabins";
+import { createUpdateCabin } from "../../services/apiCabins";
 
 export function useCreateCabin() {
   const queryClient = useQueryClient();
 
   const { mutate: createCabin, isPending: isCreating } = useMutation({
-    mutationFn: createEditCabin,
+    mutationFn: createUpdateCabin,
     onSuccess: () => {
       toast.success("New cabin was successfully created");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
